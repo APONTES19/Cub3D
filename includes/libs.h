7ab9340d0995afeced76e497e00dd86db904eece
rick <rickyamas@gmail.com>
2023-03-30 03:41:20 -0300

    norminette:libs

--- includes/libs.h
+++ includes/libs.h
@@ -6,7 +6,7 @@
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 11:55:28 by lucasmar          #+#    #+#             */
-/*   Updated: 2023/03/10 21:03:00 by ryoshio-         ###   ########.fr       */
+/*   Updated: 2023/03/30 03:40:41 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # include "../libs/mlx_linux/mlx.h"
 # include <stdio.h>
 # include <stdlib.h>
-# include <errno.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <math.h>
